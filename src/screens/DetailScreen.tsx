1cb76439aa1ba0f1ef6bb1834487bf30777b9ec9
eduar766 <eduar766@gmail.com>
2025-04-05 12:45:54 -0300

    Añadir modal para mostrar imágenes de aves en la pantalla de detalles

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -2,38 +2,54 @@ import React from 'react';
 import { ScrollView, Image } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../navigation';
-import { Card, Text, Title, Paragraph } from 'react-native-paper';
+import { Card, Text, Title, Paragraph, Modal } from 'react-native-paper';
+import { transparent } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 
 type DetailRouteProp = RouteProp<RootStackParamList, 'Detail'>;
 
 export const DetailScreen = () => {
-  const route = useRoute<DetailRouteProp>();
-  const { bird } = route.params;
+    const route = useRoute<DetailRouteProp>();
+    const { bird } = route.params;
 
-  return (
-    <ScrollView style={{ padding: 16 }}>
-      <Card style={{ marginBottom: 16 }}>
-        <Card.Cover source={{ uri: bird.images.full }} resizeMode="cover"/>
-        <Card.Title
-          title={bird.name.spanish}
-          subtitle={`${bird.name.english} / ${bird.name.latin}`}
-        />
-      </Card>
+    const [visible, setVisible] = React.useState(false);
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle = { backgroundColor: 'transparent', padding: 20 };
 
-      <Card>
-        <Card.Content>
-          <Title>Información disponible</Title>
-          <Paragraph style={{ marginTop: 8 }}>
-            <Text variant="labelMedium">ID:</Text> {bird.uid}
-          </Paragraph>
-          <Paragraph>
-            <Text variant="labelMedium">Orden:</Text> {bird.sort}
-          </Paragraph>
-          <Paragraph>
-            <Text variant="labelMedium">Enlace:</Text> {bird._links.self}
-          </Paragraph>
-        </Card.Content>
-      </Card>
-    </ScrollView>
-  );
+    return (
+        <>
+            <ScrollView style={{ padding: 16 }}>
+                <Card style={{ marginBottom: 16 }} onPress={showModal}>
+                    <Card.Cover source={{ uri: bird.images.full }} resizeMode="contain" />
+                    <Card.Title
+                        title={bird.name.spanish}
+                        subtitle={`${bird.name.english} / ${bird.name.latin}`} />
+                </Card>
+
+                <Card>
+                    <Card.Content>
+                        <Title>Información disponible</Title>
+                        <Paragraph style={{ marginTop: 8 }}>
+                            <Text variant="labelMedium">ID:</Text> {bird.uid}
+                        </Paragraph>
+                        <Paragraph>
+                            <Text variant="labelMedium">Orden:</Text> {bird.sort}
+                        </Paragraph>
+                        <Paragraph>
+                            <Text variant="labelMedium">Enlace:</Text> {bird._links.self}
+                        </Paragraph>
+                    </Card.Content>
+                </Card>
+            </ScrollView>
+
+            <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                <Image
+                    source={{ uri: bird.images.full }}
+                    style={{ width: '100%', height: '100%' }}
+                    resizeMode="contain"
+                    onTouchEnd={hideModal}
+                />
+            </Modal>
+        </>
+    );
 };
