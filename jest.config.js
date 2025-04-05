1846b99a201e6fb85ceda3b98b31b11c0d7ce06f
eduar766 <eduar766@gmail.com>
2025-04-05 19:51:09 -0300

    chore: actualizar configuración de Jest para incluir transformaciones y extensiones de archivos

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
